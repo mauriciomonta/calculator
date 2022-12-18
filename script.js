012d2bd760a19bda047adfa64b4f9b7e4fcb3a28
Mauricio <mauriciomontadg@gmail.com>
2022-12-18 00:05:00 -0300

    Copy to clipboard function added

--- script.js
+++ script.js
@@ -7,18 +7,18 @@ let finalRes;
 function firstStep(operatorSym) {
 
     numA = parseFloat(showNum.value);
-   console.log(numA);
-   showNum.value = '';
-   operator = operatorSym.value;
-   console.log(operator);
+    console.log(numA);
+    showNum.value = '';
+    operator = operatorSym.value;
+    console.log(operator);
 
-   return;
+    return;
 }
 
 function result() {
     numB = parseFloat(showNum.value);
 
-    switch(operator) {
+    switch (operator) {
         case '+':
             finalRes = numA + numB;
             break;
@@ -33,7 +33,47 @@ function result() {
             break;
     }
 
-    console.log(numA,numB);
-    console.log('res',finalRes);
+    console.log(numA, numB);
+    console.log('res', finalRes);
     showNum.value = finalRes;
+}
+
+function Copy() {
+    let copyText = document.getElementsByName("showNum");
+    let clipboard = copyText[0].value;
+
+    // Select the text field
+    focus(clipboard);
+
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(clipboard);
+
+
+    const grow = [
+        { transform: 'scale(1)' },
+        { transform: 'scale(1.03)' },
+        { transform: 'scale(1)' }
+      ];
+      
+      const growTiming = {
+        duration: 200,
+        iterations: 1,
+      }
+
+      const value = document.getElementById("showNum");
+      value.animate(grow, growTiming)
+
+
+    // Alert the copied text
+    console.log("Copied the text: " + clipboard);
+
+    // show alert
+    document.getElementById("copied").classList.add("flex");
+    setTimeout(() => {
+        document.getElementById("copied").classList.add("opacity");
+    }, 100);
+    setTimeout(() => {
+        document.getElementById("copied").classList.remove("opacity");
+        document.getElementById("copied").classList.remove("flex");
+    }, 3000);
 }
